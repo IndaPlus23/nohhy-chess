815a1b0b12526626154fba14973e0904aafaaef1
unknown <hyotylainennatan@gmail.com>
2023-09-21 11:08:21 +0200

    removed chess_lib from repo

--- chess_lib/target/debug/deps/chess_lib-2e4a5f4d14450ed7.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Natan\OneDrive - Örebro kommun\Documents\_OneDrive\rust code\nohhy-rustIntro\chess_lib\target\debug\deps\chess_lib-2e4a5f4d14450ed7.rmeta: src\lib.rs
-
-C:\Users\Natan\OneDrive - Örebro kommun\Documents\_OneDrive\rust code\nohhy-rustIntro\chess_lib\target\debug\deps\chess_lib-2e4a5f4d14450ed7.d: src\lib.rs
-
-src\lib.rs:
